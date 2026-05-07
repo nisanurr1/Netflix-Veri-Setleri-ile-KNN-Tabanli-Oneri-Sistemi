a27fb81bdf2b2f68bda14fe3f65255036cf1995e
Nisanur Cakir <nisanur@example.com>
2026-05-07 14:29:55 +0300

    bir takım veri ön işleme adımları gerçekleştirdim

--- veri_okuma.py
+++ veri_okuma.py
@@ -5,5 +5,30 @@ import pandas as pd
 film_df = pd.read_csv("data/netflix_filmler.csv")
 dizi_df = pd.read_csv("data/netflix_diziler.csv")
 
+print("Film Veri Seti")
 print(film_df.head())
+
+print("\nVeri seti bilgileri:")
+print(film_df.info())
+# info ile beraber satır sütun sayısı, veri tipleri, eksik veri var mı kontrolü yapıyorum.
+
+print("\nSütun isimleri:")
+print(film_df.columns)
+
+print("\nEksik veri sayıları:")
+print(film_df.isnull().sum())
+
+# Buraya kadar filmler içindi.
+
+print("Dizi Veri Seti")
 print(dizi_df.head())
+
+print("\nVeri seti bilgileri:")
+print(dizi_df.info())
+
+print("\nSütun isimleri:")
+print(dizi_df.columns)
+
+print("\nEksik veri sayıları:")
+print(dizi_df.isnull().sum())
+
--- veri_on_isleme.py
+++ veri_on_isleme.py
@@ -1,1 +1,24 @@
 # ekik verileri silme, gereksiz sütunları kaldırma ve gerektiği yerde tür dönüşümlerini yaptığım kısım.
+# knn algoritması sayısal verilerle çalışır.
+
+import pandas as pd
+
+film_df = pd.read_csv("data/netflix_filmler.csv")
+dizi_df = pd.read_csv("data/netflix_diziler.csv")
+
+# film veri setinde gereksiz ve fazla eksik olan verilerin temizlenmesi
+# duration'da(süre) 16000 adet boş satır bulunmakta. 
+# netflixe eklenme yılı son eklenenleri görmek açısından sonradan önemli olabilir.
+film_df = film_df.drop(columns=["show_id","duration"])
+
+# türler ve açıklamalar knn için önemli değişkenler, bu yüzden eksik olan verilerin bulunduğu satırları temizliyorum.
+film_df = film_df.dropna(subset=["genres", "description"])
+
+# aynı şekilde dizi veri setinde de tür ve açıklamalar önemli olacak.
+dizi_df = dizi_df.dropna(subset=["genres", "description"])
+dizi_df = dizi_df.drop(columns=["show_id"])
+
+print("Film Veri Seti Temizlenmiş Yeni Boyutu:")
+print(film_df.shape)
+print("Dizi Veri Seti Temizlenmiş Yeni Boyutu:")
+print(dizi_df.shape)
